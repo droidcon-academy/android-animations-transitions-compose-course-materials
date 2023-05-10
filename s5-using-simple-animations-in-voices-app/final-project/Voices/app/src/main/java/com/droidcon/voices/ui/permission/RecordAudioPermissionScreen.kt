6c24241b0dbec8f4075e482ed02425a10d2c280f
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2023-05-10 09:14:25 +0330

    Fix

--- s5-using-simple-animations-in-voices-app/final-project/Voices/app/src/main/java/com/droidcon/voices/ui/permission/RecordAudioPermissionScreen.kt
+++ s5-using-simple-animations-in-voices-app/final-project/Voices/app/src/main/java/com/droidcon/voices/ui/permission/RecordAudioPermissionScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -35,7 +36,7 @@ fun RecordAudioPermissionScreen(
     modifier: Modifier = Modifier,
     onRequestMicPermission: () -> Unit
 ) {
-    val show = MutableTransitionState(initialState = false)
+    val show = remember { MutableTransitionState(initialState = false) }
     show.targetState = true
 
 
