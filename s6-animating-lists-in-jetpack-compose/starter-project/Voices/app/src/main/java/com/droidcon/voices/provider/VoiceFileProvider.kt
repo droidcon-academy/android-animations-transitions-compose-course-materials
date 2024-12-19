d66c94c4a246c151a35ba08c44e0a2b2353efa86
Musab Nasredlin <musabagab2010@gmail.com>
2024-12-19 23:23:24 +0400

    Updated projects

--- s6-animating-lists-in-jetpack-compose/starter-project/Voices/app/src/main/java/com/droidcon/voices/provider/VoiceFileProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.droidcon.voices.provider
-
-import androidx.core.content.FileProvider
-import com.droidcon.voices.R
-
-/**
- * FileProvider to enable sharing of voice files with other apps
- */
-class VoiceFileProvider : FileProvider(R.xml.file_paths)
